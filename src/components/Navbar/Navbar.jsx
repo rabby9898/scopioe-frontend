da5d5d5bbdd2f9ec18d9a3a2c51d1fdebe76c53d
rabby9898 <mdfajlerabby00@gmail.com>
2024-08-03 14:59:39 +0600

    Sidebar: Added for small device

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,79 @@
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { IoIosArrowDown, IoMdNotificationsOutline } from "react-icons/io";
 import { LuLogOut } from "react-icons/lu";
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
+import useWindowSize from "../../Hooks/useWindowSize";
+import logo from "../../assets/logo.png";
+import SidebarSm from "./SIdebarSm"; // Import the SidebarSm component
+
 const Navbar = () => {
+  const { width } = useWindowSize();
+  const isSmallDevice = width < 768;
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <nav className=" top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700 ">
-      <div className="px-3 py-3 lg:px-5 lg:pl-3">
-        <div className="px-5 flex items-center justify-between">
-          <div>
-            <div className="flex items-center gap-4">
-              <FaUserCircle className="w-10 h-10 rounded-full" />
-              <div className="font-medium dark:text-white">
-                <div className="flex justify-between items-center">
-                  <p className="text-sm text-[#152A16]">Shagor Ahmed</p>
-                  <IoIosArrowDown />
+    <>
+      <nav className="top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="px-5 flex items-center justify-between">
+            <div>
+              {isSmallDevice ? (
+                <img className="w-[110px] h-[30px]" src={logo} alt="Logo" />
+              ) : (
+                <div className="flex items-center gap-4">
+                  <FaUserCircle className="w-10 h-10 rounded-full" />
+                  <div className="font-medium dark:text-white">
+                    <div className="flex justify-between items-center">
+                      <p className="text-sm text-[#152A16]">Shagor Ahmed</p>
+                      <IoIosArrowDown />
+                    </div>
+                    <p className="text-sm text-[#5C635A] dark:text-gray-400 font-normal">
+                      shagor@gmail.com
+                    </p>
+                  </div>
                 </div>
-                <p className="text-sm text-[#5C635A] dark:text-gray-400 font-normal">
-                  shagor@gmail.com
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-between items-center">
-            <div className=" text-black p-3 rounded-full border border-[#E7E7E7]">
-              <IoMdNotificationsOutline className="text-xl" />
+              )}
             </div>
 
-            <div className="divider divider-horizontal"></div>
-
-            <div className="flex justify-start gap-5 items-center">
-              <button className="text-[#F15E4A] text-base">Log Out</button>
-              <div className="p-3 rounded-full bg-[#FFECEA]">
-                <LuLogOut className="text-[#F15E4A] text-xl " />
+            {isSmallDevice ? (
+              <div className="flex justify-between items-center gap-5">
+                <div className="text-black p-3 rounded-full border border-[#E7E7E7]">
+                  <IoMdNotificationsOutline className="text-xl" />
+                </div>
+                <HiMiniBars3BottomRight
+                  className="text-black text-2xl"
+                  onClick={toggleSidebar}
+                />
               </div>
-            </div>
+            ) : (
+              <div className="flex justify-between items-center">
+                <div className="text-black p-3 rounded-full border border-[#E7E7E7]">
+                  <IoMdNotificationsOutline className="text-xl" />
+                </div>
+
+                <div className="divider divider-horizontal"></div>
+
+                <div className="flex justify-start gap-5 items-center">
+                  <button className="text-[#F15E4A] text-base">Log Out</button>
+                  <div className="p-3 rounded-full bg-[#FFECEA]">
+                    <LuLogOut className="text-[#F15E4A] text-xl " />
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
+      </nav>
+      <div className="">
+        {isSidebarOpen && <SidebarSm toggleSidebar={toggleSidebar} />}
       </div>
-    </nav>
+    </>
   );
 };
 
---
+++ src/components/Navbar/SIdebarSm.jsx
@@ -0,0 +1,105 @@
+const SidebarSm = ({ toggleSidebar }) => {
+  return (
+    <div className="fixed inset-0 z-50  flex justify-end flex-row-reverse">
+      <div className=" w-2/3 md:w-1/3">
+        <div className="flex flex-col items-center bg-[#156BCA] justify-end py-16">
+          <img
+            className="w-24 h-24 rounded-full"
+            src="https://via.placeholder.com/100"
+            alt="User"
+          />
+          <h2 className="text-white text-xl font-bold mt-2">Sagar Soni</h2>
+          <p className="text-black text-sm">sagar@gmail.com</p>
+        </div>
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800 pt-5">
+          <ul className="space-y-2 font-medium">
+            <li className="">
+              <a
+                href="/"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#D4E9FF] dark:hover:bg-gray-700 group"
+              >
+                <img src="https://i.ibb.co/fNLw56H/home.png" alt="" />
+                <span className="ms-3 text-[#5C635A] text-base font-normal">
+                  Home
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#D4E9FF] dark:hover:bg-gray-700 group"
+              >
+                <img src="https://i.ibb.co/jyQKKJD/new.png" alt="" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-[#5C635A] text-base font-normal">
+                  New Listing
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#D4E9FF] dark:hover:bg-gray-700 group"
+              >
+                <img src="https://i.ibb.co/pWGfgXP/Search.png" alt="" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-[#5C635A] text-base font-normal">
+                  Search
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#D4E9FF] dark:hover:bg-gray-700 group"
+              >
+                <img src="https://i.ibb.co/7K37Cv3/Paper.png" alt="" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-[#5C635A] text-base font-normal">
+                  About
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#D4E9FF] dark:hover:bg-gray-700 group mb-5"
+              >
+                <img src="https://i.ibb.co/48DMrm8/Vector.png" alt="" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-[#5C635A] text-base font-normal">
+                  Favorites
+                </span>
+              </a>
+            </li>
+            <hr className="my-10 py-[1px] h-0.5 border-t-0 bg-[#E7E7E7] dark:bg-white/10" />
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#D4E9FF] dark:hover:bg-gray-700 group mt-5"
+              >
+                <img src="https://i.ibb.co/3zKYZmv/info.png" alt="" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-[#5C635A] text-base font-normal">
+                  Help Center
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#D4E9FF] dark:hover:bg-gray-700 group"
+              >
+                <img src="https://i.ibb.co/b53KmBg/Setting.png" alt="" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-[#5C635A] text-base font-normal">
+                  Setting
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div
+        className="bg-black opacity-50 w-1/3 md:w-2/3 h-full"
+        onClick={toggleSidebar}
+      ></div>
+    </div>
+  );
+};
+
+export default SidebarSm;
